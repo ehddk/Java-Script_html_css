9d0827edcb3f5c64ada1dce28e7bdc3aea0dddab
Dongah <bj9147@naver.com>
2022-08-10 20:23:09 +0900

    second commit

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //만약 스트링이 2개이상 쓰이면 아예 상수로 만들어서 쓰는게 나음.
 const USERNAME_KEY = "username"; //반복되는 부분이라 오타날까봐 따로 const로 생성!! 습관들이기
+
 function onSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME); //form은 사라짐.
+  event.preventDefault(); //event의 기본동작인 새로고침을 막음
+  loginForm.classList.add(HIDDEN_CLASSNAME); //form을 숨김.
   const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username); //유저가 이름을 제출하면 그것을 저장! setItem(이름, 변수명);
+  localStorage.setItem(USERNAME_KEY, username); //유저가 \이름을 제출하면 그것을 저장! setItem(key, value);
   //greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
 function paintGreetings(username) {
@@ -29,3 +29,4 @@ if (savedUsername === null) {
   //show the greetings
   paintGreetings(savedUsername);
 }
+//user정보가 없다면 form의 submit을 기다린 다음 INPUT으로부터 유저정보를 받고 INPUT에서 받은 user를 가진 paintGreetings를 호출.
